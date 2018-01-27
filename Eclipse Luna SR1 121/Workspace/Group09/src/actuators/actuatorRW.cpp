45d5e232ffd80b4aedcf7a3ae8187db27a924eea
felixhessinger <33756846+felixhessinger@users.noreply.github.com>
2018-01-27 13:44:52 +0100

    added: connection to the reactionWheel speed controller

--- Eclipse Luna SR1 121/Workspace/Group09/src/actuators/actuatorRW.cpp
+++ Eclipse Luna SR1 121/Workspace/Group09/src/actuators/actuatorRW.cpp
@@ -20,16 +20,28 @@ using namespace std;
 
 using std::string;
 
-HAL_GPIO HBRIDGE_A_INA(GPIO_036); /* declare HAL_GPIO for GPIO_036 = PC4 (HBRIDGE-A INA pin) */
-HAL_GPIO HBRIDGE_A_INB(GPIO_017); /* declare HAL_GPIO for GPIO_017 = PB1 (HBRIDGE-B INA pin) */
+HAL_GPIO HBRIDGE_A_INA(GPIO_036); /* -declare HAL_GPIO for GPIO_036 = PC4 (A-HBRIDGE-A INA pin) */
+HAL_GPIO HBRIDGE_A_INB(GPIO_017); /* declare HAL_GPIO for GPIO_017 = PB1 (A-HBRIDGE-B INA pin) */
+//HAL_GPIO HBRIDGE_A_INA(GPIO_016); /* -declare HAL_GPIO for GPIO_036 = PB0 (B-HBRIDGE-A INA pin) */
+//HAL_GPIO HBRIDGE_A_INB(GPIO_071); /* declare HAL_GPIO for GPIO_017 = PE7 (B-HBRIDGE-B INA pin) */
+
+
+//HAL_GPIO HBRIDGE_A_INA(GPIO_076); /* declare HAL_GPIO for GPIO_076 = PE12 (D-HBRIDGE-A INA pin) */
+//HAL_GPIO HBRIDGE_A_INB(GPIO_079); /* declare HAL_GPIO for GPIO_079 = PE15 (D-HBRIDGE-B INA pin) */
 HAL_GPIO HBRIDGE_EN(GPIO_066); /* declare HAL_GPIO for GPIO_066 = PE2 (HBRIDGE Power Enable pin) */
 HAL_PWM ReactionWheel(PWM_IDX12); /* declare HAL_PWM for PWM_IDX12 = TIM4-CH1 (HBRIDGE-A), please refer to hal_pwm.h for correct PWM mapping*/
 
 ActuatorRW actuatorRW;
 
 CommBuffer<TelecommandData> ActuatorRW_TelecommandDataBuffer;
-Subscriber ActuatorRW_TelecommandDataSubscriber(TelecommandDataTopic,
-		ActuatorRW_TelecommandDataBuffer);
+Subscriber ActuatorRW_TelecommandDataSubscriber(TelecommandDataTopic, ActuatorRW_TelecommandDataBuffer);
+
+CommBuffer<MotorData> MotorDataBuffer2;
+Subscriber MotorDataSubscriber2(MotorDataTopic, MotorDataBuffer2);
+
+CommBuffer<SurveillanceData> SurveillanceDataBuffer3;
+Subscriber SurveillanceDataSubscriber3(SurveillanceDataTopic, SurveillanceDataBuffer3);
+
 
 ActuatorRW::ActuatorRW() {
 	dir = 0; //cw
@@ -41,6 +53,7 @@ void ActuatorRW::init() {
 	HBRIDGE_A_INA.init(true, 1, 1); /* initialization of the HAL object should be called one time only in the project*/
 	HBRIDGE_A_INB.init(true, 1, 0); /* initialization of the HAL object should be called one time only in the project*/
 	ReactionWheel.init(5000, 1000); /* initialization of the HAL object should be called one time only in the project*/
+	//ReactionWheel.init(10000, 8400);
 	//ReactionWheel.write(250); /* Set the Duty Cycle to 25% */
 
 }
@@ -53,38 +66,82 @@ void ActuatorRW::run() {
 	//ReactionWheel.write(1);
 	while (1) {
 
+		MotorData motorData;
+		MotorDataBuffer2.get(motorData);
+
 		TelecommandData telecommandData;
 		ActuatorRW_TelecommandDataBuffer.get(telecommandData);
 
 		vector<string> splitted = split(telecommandData.telecommand.c_str(), ';');
 		dir = atoi(splitted[2].c_str()) == 0 ? 0 : 1;
 		m_speed = atoi(splitted[3].c_str());
-		PRINTF("\ndir:%d, m_speed:%d", dir, m_speed);
+		//PRINTF("\ndir:%d, m_speed:%d", dir, m_speed);
 
 		//ReactionWheel.write(200);
-		if (dir == 0) {
+		int speed = (int) (motorData.controlled_m_speed*0.9);
+		//int speed1 = (int) (surveillanceData.asdf*8.4); // should be set to 8.4 for full range
+		if ((motorData.controlled_m_speed>0)&& (motorData.controlled_m_speed<=1000)) {
 			//calm motor before direction is changed
 
 			//suspendCallerUntil(NOW()+10*MILLISECONDS);
 			//change direction
 			HBRIDGE_A_INB.setPins(0);
+			HBRIDGE_A_INA.setPins(0);
+			suspendCallerUntil(NOW()+1*MILLISECONDS);
+			HBRIDGE_A_INB.setPins(0);
 			HBRIDGE_A_INA.setPins(1);
-			ReactionWheel.write(m_speed);
-		} else if (dir == 1) {
+			speed--;
+			//PRINTF("speedA %d",speed);
+			ReactionWheel.write(speed);
+		} else if ((motorData.controlled_m_speed<0) && (motorData.controlled_m_speed>=-1000)) {
 			//calm motor before direction is changed
 			//ReactionWheel.write(1);
-			//suspendCallerUntil(NOW()+10*MILLISECONDS);
+			//suspendCallerUntil(NOW()+10*MILLISECONDS) ;
 			//change direction
+			HBRIDGE_A_INB.setPins(0);
+			HBRIDGE_A_INA.setPins(0);
+			suspendCallerUntil(NOW()+1*MILLISECONDS);
+
 			HBRIDGE_A_INB.setPins(1);
 			HBRIDGE_A_INA.setPins(0);
-			ReactionWheel.write(m_speed);
+			speed++;
+			int _speed = abs(speed);
+			//PRINTF("speedB %d",_speed);
+
+			ReactionWheel.write(_speed);
 		} else {
+			PRINTF("speedC %d",speed);
 			HBRIDGE_A_INB.setPins(0);
 			HBRIDGE_A_INA.setPins(0);
 			//ReactionWheel.write(200);
 		}
+		suspendCallerUntil(NOW()+10*MILLISECONDS);
+
+
+
+//		if (dir == 0) {
+//			//calm motor before direction is changed
+//
+//			//suspendCallerUntil(NOW()+10*MILLISECONDS);
+//			//change direction
+//			HBRIDGE_A_INB.setPins(0);
+//			HBRIDGE_A_INA.setPins(1);
+//			ReactionWheel.write(m_speed);
+//		} else if (dir == 1) {
+//			//calm motor before direction is changed
+//			//ReactionWheel.write(1);
+//			//suspendCallerUntil(NOW()+10*MILLISECONDS);
+//			//change direction
+//			HBRIDGE_A_INB.setPins(1);
+//			HBRIDGE_A_INA.setPins(0);
+//			ReactionWheel.write(m_speed);
+//		} else {
+//			HBRIDGE_A_INB.setPins(0);
+//			HBRIDGE_A_INA.setPins(0);
+//			//ReactionWheel.write(200);
+//		}
 		//ReactionWheel.write(250); /* Set the Duty Cycle to 25% */
-		suspendCallerUntil(NOW()+1000*MILLISECONDS);
+
 	}
 }
 
