eed2c184d1106c489881fd55ef8559d2bcb21480
felixhessinger <33756846+felixhessinger@users.noreply.github.com>
2018-02-06 01:27:14 +0100

    anglePositionRedefinition_Mode0 anglePositionRedefinition_Mode2 instead of anglePositionRedefinition

--- Eclipse Luna SR1 121/Workspace/Group09/lib/control/mode2.h
+++ Eclipse Luna SR1 121/Workspace/Group09/lib/control/mode2.h
@@ -22,11 +22,13 @@ private:
 
 
 public:
-	Modes modes2;
+
 	ControlData controlData;
 	SensorData sensorData;
 	FusedData fusedData;
 	TelecommandData telecommandData;
+	Modes modes2;
+	IRData iRData;
 	Mode2();
 	void init();
 	void run();
@@ -41,12 +43,13 @@ public:
 	void set_cmd_Limits_ap(double Min_ap,double Max_ap);
 	void turn(double angle);
 // angle/position redefinition for control
-	void anglePositionRedefinition();
+	void anglePositionRedefinition_Mode0();
 // functions for linear distance control
 	void compute_linp();
 	void set_PID_values_linp(double Kp_linp, double Ki_linp,double Kd_linp);
 	void set_Sample_Time_linp(double NewSampleTime_linp);
 	void set_cmd_Limits_linp(double Min_linp,double Max_linp);
+	void anglePositionRedefinition_Mode2();
 
 
 };
